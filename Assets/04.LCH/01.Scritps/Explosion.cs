8be5d2fd9605068a0ea5291d685597cc68059e89
1bio <leeckdgns07@gmail.com>
2024-05-21 14:32:03 +0900

    LCH

---
+++ Assets/04.LCH/01.Scritps/Explosion.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosion;
+
+    float delay;
+
+    bool isExplode;
+
+
+    void Start()
+    {
+        delay = 0;
+        isExplode = false;
+    }
+
+    void Update()
+    {
+        delay += Time.deltaTime;
+        if (!isExplode)
+        {
+            Explode();
+        }
+    }
+
+
+    void Explode()
+    {
+        // delay가 5초 이상되면
+        if(delay >= 3f)
+        {
+            Boom();
+            isExplode = true;
+        }
+    }
+
+    void Boom()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
